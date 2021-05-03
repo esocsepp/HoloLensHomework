9640977b7075102851e6166bb151524cbcc49c9b
esocsepp <47364171+esocsepp@users.noreply.github.com>
2021-05-03 13:02:16 +0200

    init

---
+++ BuildingBlocks/Assets/Scripts/BlockPlacer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPlacer : MonoBehaviour
+{
+    public GameObject prefab;
+    public void PlaceBlock()
+    {
+        Instantiate(prefab, new Vector3(1f,1f,0f), Quaternion.identity);
+    }
+}
